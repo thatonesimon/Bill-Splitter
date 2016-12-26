123584ee528f364bf9706e22b2bffd8b66fc169d
Simon Wong <sswong@umail.ucsb.edu>
2016-12-26 00:49:01 -0800

    Add people screen somewhat working woo

--- app/src/main/java/com/sswong/billsplitter/MainActivity.java
+++ app/src/main/java/com/sswong/billsplitter/MainActivity.java
@@ -7,13 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText name;
-    private static ArrayList<Person> people;
+    private static ArrayList<String> people;
     private TextView names;
 
     @Override
@@ -21,19 +22,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        people = new ArrayList<>(0);
+
         name = (EditText)findViewById(R.id.names);
         names = (TextView)findViewById(R.id.people);
 
     }
 
     public void addPerson(View v){
-        Person p = new Person(name.getText().toString());
-        people.add(p);
+        String newName = name.getText().toString().trim();
+        if(newName.equals("")){
+            Toast.makeText(this, "Enter a name", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(people.contains(newName)){
+            Toast.makeText(this, "Name is already in the list", Toast.LENGTH_LONG).show();
+            return;
+        }
+        people.add(newName);
         String allNames = "";
-        for(Person person:people){
+        for(String person:people){
             allNames += person + "\n";
         }
         names.setText(allNames);
+        name.setText("");
     }
 
     public void finish(View v){
