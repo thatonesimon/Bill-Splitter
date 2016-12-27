fd51e2063b6f8b4f00a050a15e9490f5d4c55101
Simon Wong <sswong@umail.ucsb.edu>
2016-12-26 21:22:46 -0800

    having trouble starting a new activity...

--- app/src/main/java/com/sswong/billsplitter/AddItems.java
+++ app/src/main/java/com/sswong/billsplitter/AddItems.java
@@ -1,17 +1,77 @@
 package com.sswong.billsplitter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Simon on 12/25/2016.
  */
-public class AddItems extends Activity{
+public class AddItems extends AppCompatActivity {
+
+    private EditText name;
+    private EditText price;
+    private TextView itemList;
+
+    private ArrayList<Person> people;
+    private ArrayList<Item> items;
+
+    private final static String TAG = "BillSplitter";
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_items);
 
+        name = (EditText) findViewById(R.id.itemName);
+        price = (EditText) findViewById(R.id.itemPrice);
+        itemList = (TextView) findViewById(R.id.itemList);
+
+        people = new ArrayList<>(0);
+        items = new ArrayList<>(0);
+
+        // get names from MainActivity and put into ArrayList of people
+        Intent intent = getIntent();
+        ArrayList<String> names = intent.getExtras().getStringArrayList("PEOPLE");
+        for(String n:names){
+            Person p = new Person(n);
+            people.add(p);
+        }
+
     }
+
+    public void addItem(View v){
+        String newItemName = name.getText().toString().trim();
+        if(newItemName.equals("")){
+            Toast.makeText(this, "Enter a valid item name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        float newItemPrice = Float.valueOf(price.getText().toString());
+        if(newItemPrice <= 0){
+            Toast.makeText(this, "Enter a valid price", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Item i = new Item(newItemName,newItemPrice,people);
+        items.add(i);
+
+        // update itemList
+        if(items.size()==1){
+            itemList.setText(newItemName);
+        }
+        itemList.setText(itemList.getText()+"\n"+newItemName);
+
+        Log.d(TAG, "Item added to the list");
+        name.setText("");
+        price.setText("");
+    }
+
 }
--- app/src/main/java/com/sswong/billsplitter/MainActivity.java
+++ app/src/main/java/com/sswong/billsplitter/MainActivity.java
@@ -3,6 +3,7 @@ package com.sswong.billsplitter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,42 +15,56 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private EditText name;
-    private static ArrayList<String> people;
     private TextView names;
+
+    private static ArrayList<String> people;
+
+    private final static String TAG = "BillSplitter";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        people = new ArrayList<>(0);
-
         name = (EditText)findViewById(R.id.names);
         names = (TextView)findViewById(R.id.people);
+
+        people = new ArrayList<>(0);
 
     }
 
     public void addPerson(View v){
         String newName = name.getText().toString().trim();
         if(newName.equals("")){
-            Toast.makeText(this, "Enter a name", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Enter a name", Toast.LENGTH_SHORT).show();
             return;
         }
         if(people.contains(newName)){
-            Toast.makeText(this, "Name is already in the list", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Name is already in the list", Toast.LENGTH_SHORT).show();
             return;
         }
         people.add(newName);
-        String allNames = "";
-        for(String person:people){
-            allNames += person + "\n";
+
+        // update names
+        if(people.size()==1){
+            names.setText(newName);
         }
-        names.setText(allNames);
+        else {
+            names.setText(names.getText() + "\n" + newName);
+        }
+
+        Log.d(TAG, "Name added to the list");
         name.setText("");
     }
 
     public void finish(View v){
+        if(people.size() < 2){
+            Toast.makeText(this, "You need at least 2 people to split a bill.", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.d(TAG, "Finished adding names, moving onto adding items");
         Intent intent = new Intent(MainActivity.this, AddItems.class);
+        intent.putExtra("com.sswong.billsplitter.PEOPLE", people);
         startActivity(intent);
     }
 
